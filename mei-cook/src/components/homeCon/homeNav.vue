c4954bb71141dcb757771ef9b31fdb15ad9ee984
Vichede91 <851517199@qq.com>
2019-01-24 17:01:38 +0800

    新建项目

---
+++ mei-cook/src/components/homeCon/homeNav.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="nav">
+        <h1>导航</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+#nav{
+      width:100%;height:30px;
+     position:absolute;top:44px;left:0;
+}
+</style>
+
---
+++ mei-cook/src/pages/discount/index.vue
@@ -0,0 +1,11 @@
+<template>
+<div id="category" class="page">
+    <h1>优惠</h1>
+</div>
+</template>
+
+<script>
+
+export default {
+}   
+</script>
